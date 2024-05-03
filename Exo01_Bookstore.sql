380025fff7549fde61e0fd23bf29caaf57a8ae03
Said Laouedj <111045941+LaouedjSaid@users.noreply.github.com>
2024-05-03 12:34:26 +0100

    Add files via upload

---
+++ Exo01_Bookstore.sql
@@ -0,0 +1,42 @@
+
+create database bookstore -- To create database their name bookstore
+use bookstore
+create table books( -- To create table their name employer 
+bookid int primary key, -- To create a colummn named employerid 
+title varchar(50), -- To create a colummn named first_name
+authorid varchar(50), -- To create a colummn named last_name
+categoryid varchar(50), -- To create a colummn named department 
+price decimal(10,2) -- To create a colummn named department 
+);
+create table authors( -- To create table their name employer 
+authorid int primary key, -- To create a colummn named employerid 
+authorname varchar(50), -- To create a colummn named last_name
+);
+create table categories( -- To create table their name employer 
+categoryid int primary key, -- To create a colummn named employerid 
+categoryname varchar(50), -- To create a colummn named first_name
+);
+
+
+insert into books(bookid,title,authorid,categoryid,price)
+values(1,'titleA','authorA','1','100'),
+(2,'titleB','authorB','2','200'),
+(3,'titleC','authorC','1','200'),
+(4,'titleD','authorD','2','200');
+
+
+insert into authors(authorid,authorname)
+values(1,'nameA'),
+(2,'nameB'),
+(3,'nameC'),
+(4,'nameD');
+
+insert into categories(categoryid,categoryname)
+values(1,'fiction'),
+(2,'scientific'),
+(3,'non fiction');
+
+
+update books -- To upddate our table employee 
+set price = price*1.1 -- To change the column department "marketing" by "technique"
+where categoryid = 1 -- for all the columns = 1
